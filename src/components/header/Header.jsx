e2b85ba00d50b0f138ae244cb45f0a21e84fbf3b
Jose Pereira <josepsneto@gmail.com>
2022-02-17 19:26:11 -0300

    feat: adds login controls to Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import Styles from './Header.module.css'
+import UserIcon from '../../assets/icons/user.png'
+import LogoutIcon from '../../assets/icons/log-out.png'
 
 const Header = () => {
   return (
     <div className={Styles.headerWrap}>
       <div className={Styles.logo}>Controle de Patrimônio</div>
-      <ul className={Styles.links}>
-        <li>Acervo</li>
-        <li>Usuários</li>
-        <li>Inventário</li>
-      </ul>
+      <div className={Styles.linksWrap}>
+        <ul className={Styles.links}>
+          <li>Acervo</li>
+          <li>Usuários</li>
+          <li>Inventário</li>
+        </ul>
+        <ul className={Styles.loginControls}>
+          <li>
+            <img className={Styles.userIcon} src={UserIcon} alt='Icone de usuário' />
+            <span>Jose</span>
+          </li>
+          <li>
+            <img className={Styles.logoutIcon} src={LogoutIcon} alt='Icone de usuário' />
+            <span>Sair</span>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }

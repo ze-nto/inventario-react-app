0415ede2929b6b747007093037f7450998283ee7
Jose Pereira <josepsneto@gmail.com>
2022-02-18 09:59:22 -0300

    fix: fits code stayles to standard

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import Login from './Login/Login.jsx';
-import Search from './Search/Search.jsx';
+import Login from './Login/Login.jsx'
+import Search from './Search/Search.jsx'
 
 export {
   Login,

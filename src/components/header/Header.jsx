b2f54410d1c2a4aec309587c6a22006c7207f235
Jose Pereira <josepsneto@gmail.com>
2022-02-18 10:04:32 -0300

    feat: adds Header links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import Styles from './Header.module.css'
 import UserIcon from '../../assets/icons/user.png'
 import LogoutIcon from '../../assets/icons/log-out.png'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div className={Styles.headerWrap}>
-      <div className={Styles.logo}>Controle de Patrimônio</div>
+    <header className={Styles.headerWrap}>
+      <Link to='/'><div className={Styles.logo}>Controle de Patrimônio</div></Link>
       <div className={Styles.linksWrap}>
         <ul className={Styles.links}>
-          <li>Acervo</li>
-          <li>Usuários</li>
-          <li>Inventário</li>
+          <li><Link to='/acervo'>Acervo</Link></li>
+          <li><Link to='/usuarios'>Usuários</Link></li>
+          <li><Link to='/inventario'>Inventário </Link></li>
         </ul>
         <ul className={Styles.loginControls}>
           <li>
@@ -24,7 +25,7 @@ const Header = () => {
           </li>
         </ul>
       </div>
-    </div>
+    </header>
   )
 }
 

38aec3057ba100542c076d4936b35b7bc779c529
Jose Pereira <josepsneto@gmail.com>
2022-02-18 15:33:16 -0300

    feat: implements login logic functions

--- src/pages/login/components/LoginForm/LoginForm.jsx
+++ src/pages/login/components/LoginForm/LoginForm.jsx
@@ -1,49 +1,52 @@
 
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { TOKEN_POST, USER_GET } from '../../../../api/api.js'
+import Button from '../../../../components/form/Button/Button.jsx'
+import TextInput from '../../../../components/form/TextInput/TextInput.jsx'
+import useForm from '../../../../hooks/useForm.js'
 import Styles from './LoginForm.module.css'
 
 const LoginForm = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const username = useForm('email')
+  const password = useForm()
 
-  const handleSubmit = (event) => {
+  useEffect(() => {
+    const token = window.localStorage.getItem('token')
+    if (token) {
+      getUser(token)
+    }
+  }, [])
+
+  async function getUser (token) {
+    const { url, options } = USER_GET(token)
+    const response = await fetch(url, options)
+    const json = await response.json()
+    console.log(json)
+  }
+
+  const handleSubmit = async (event) => {
     event.preventDefault()
-    fetch('http://localhost/wordpress/controle_patrimonio/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ username, password })
-    }).then(response => {
-      console.log(response)
-      return response.json()
-    }).then(json => console.log(json))
+
+    if (username.validate() && password.validate()) {
+      const { url, options } = TOKEN_POST({ username: username.value, password: password.value })
+      const response = await fetch(url, options)
+      const json = await response.json()
+
+      window.localStorage.setItem('token', json.token)
+      getUser(json.token)
+    }
   }
 
   return (
     <section className={Styles.loginWrap}>
       <h2>Login</h2>
       <form onSubmit={handleSubmit} className={Styles.loginForm}>
-        <div className={Styles.inputGroup}>
-          <label htmlFor='email'>Email</label>
-          <input
-            type='email'
-            name='email'
-            value={username}
-            onChange={({ target }) => setUsername(target.value)}
-          />
+        <TextInput label='Email' type='email' name='username' {...username} />
+        <TextInput label='Senha' type='password' name='password' {...password} />
+        <div className={Styles.buttonWrap}>
+          <Button>Entrar</Button>
         </div>
-        <div className={Styles.inputGroup}>
-          <label htmlFor='password'>Senha</label>
-          <input
-            type='password'
-            name='password'
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button>Entrar</button>
       </form>
       <Link className={Styles.link} to='/login/recuperar-senha'>Perdeu a senha?</Link>
     </section>
